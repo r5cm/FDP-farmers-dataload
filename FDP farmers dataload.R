cb2f2f65051a9fcef8e4e490eb180805fb6d563c
ramirocadavid <ramirocadavid@gmail.com>
2017-10-06 14:13:09 -0500

    Completo, antes de primera carga

--- FDP farmers dataload.R
+++ FDP farmers dataload.R
@@ -1,67 +1,69 @@
 
 # Initial inspection of Excel files ---------------------------------------
 
-# - Make sure the sheet that contains data is named 'Template for data load'
-# - Make sure that there are no old files in the 'To be loaded' folder
+# - Make sure the sheet that contains data is named 'Template for data to.load'
+# - Make sure that there are no old files in the 'To be to.loaded' folder
 # - Do a quick visual inspection of the file
 # - Make sure there are no hidden rows or columns
 # - Make sure that data start in cell A1
 
 
-# Import load to R ---------------------------------------------------------
+# Import to.load to R ---------------------------------------------------------
 
-# Create list with files to load
-setwd("../To be loaded")
-to.load <- list.files(pattern = ".xlsx")
+# Create list with files to to.load
+setwd("../2. To be loaded")
+f.to.load <- list.files(pattern = ".xlsx")
 
 # Import metadata of data to import
 library(googlesheets)
 library(dplyr)
 (my_sheets <- gs_ls())
 template.info <- gs_title("FDP - Data load template")
-questions.coding <- template.info %>% gs_read(ws = "Questions coding", 
+# Questions coding
+q.coding <- template.info %>% gs_read(ws = "Questions coding", 
                                               range = "A1:D57" , colnames =TRUE)
+# Options
+q.options <- template.info %>% gs_read(ws = "Questions options",
+                                       range = "A1:B25", colnames = TRUE)
 
-# Import files to load
-for (i in 1:length(to.load)) {
-     temp.load <- read.xlsx(to.load[i], sheetName = "Template for data load",
-                          colClasses = questions.coding$r.class,
+# Import files to to.load
+library(xlsx)
+for (i in 1:length(f.to.load)) {
+     temp.to.load <- read.xlsx(f.to.load[i], sheetName = "Template for data load",
+                          colClasses = q.coding$r.class,
                           stringsAsFactors = FALSE)
-     names(temp.load) <- questions.coding$short.name
-     temp.load <- temp.load[rowSums(is.na(temp.load)) != ncol(temp.load), ]
-     temp.load <- data.frame(file = rep(to.load[i], nrow(temp.load)), temp.load)
-     ifelse(i == 1, load <- temp.load, load <- rbind(load, temp.load))
+     names(temp.to.load) <- q.coding$short.name
+     temp.to.load <- temp.to.load[rowSums(is.na(temp.to.load)) != ncol(temp.to.load), ]
+     temp.to.load <- data.frame(file = rep(to.load[i], nrow(temp.to.load)), temp.to.load)
+     ifelse(i == 1, to.load <- temp.to.load, to.load <- rbind(to.load, temp.to.load))
+     rm(temp.to.load)
 }
 
 
 # Clean data --------------------------------------------------------------
 
-# Check variables class
-sapply(load, class)
-# Check variables summary
-library(Hmisc)
-sapply(load, summary)
 
 # Check that all required variables have information for all observations
 # farmer name
-nb.farmer.name <- length(load$famer.name[!is.na(load$famer.name)])
-ifelse(nrow(load) == nb.farmer.name, "No blank fields",
+nb.farmer.name <- length(!is.na(to.load$farmer.name))
+ifelse(nrow(to.load) == nb.farmer.name, "No blank fields",
        warning("Encountered blank fields. Make sure there are no missing fields.",
                call. = FALSE))
+View(to.load[is.na(to.load$farmer.name), ]) # See blanks
 # Assigned to
-nb.assigned.to <- length(load$assigned.to[!is.na(load$assigned.to)])
-ifelse(nrow(load) == nb.assigned.to, "No blank fields",
+nb.assigned.to <- length(!is.na(to.load$assigned.to))
+ifelse(nrow(to.load) == nb.assigned.to, "No blank fields",
        warning("Encountered blank fields. Make sure there are no missing fields.",
                call. = FALSE))
 # Farmer code
-nb.farmer.code <- length(load$farmer.code[!is.na(load$farmer.code)])
-ifelse(nrow(load) == nb.farmer.code, "No blank fields",
+nb.farmer.code <- length(!is.na(to.load$farmer.code))
+ifelse(nrow(to.load) == nb.farmer.code, "No blank fields",
        warning("Encountered blank fields. Make sure there are no missing fields.",
                call. = FALSE))
 
 # Check that farmer codes in the files are unique
-unique.codes <- length(unique(load$farmer.code))
-ifelse(nrow(load) == unique.codes, "All codes in files to load are unique",
+unique.codes <- length(unique(to.load$farmer.code))
+ifelse(nrow(to.load) == unique.codes, "All farmer codes are unique",
        warning("Duplicate codes found. Make sure that farmer codes are unique",
                call. = FALSE))
 
@@ -71,109 +73,144 @@ library(RForcecom)
 username <- "admin@utzmars.org"
 password <- "gfutzmars2017**oyCqeCwuJQCCfOBACJKmKOIr8"
 session <- rforcecom.login(username, password)
-# Retrieve existing farmer codes and check if exist in data to load
+# Retrieve existing farmer codes and check if exist in data to to.load
 sf.codes <- rforcecom.retrieve(session, "FDP_submission__c", "farmerCode__c")
-repeat.codes <- load$farmer.code %in% sf.codes$farmerCode__c
-ifelse(length(repeat.codes[repeat.codes == TRUE]) == 0,
-                   "No repeated codes found in Salesforce",
-                   warning("At least one code already found in Salesforce",
-                           call. = FALSE))
+ifelse(sum(to.load$farmer.code %in% sf.codes$farmerCode__c) == 0,
+       "No repeated farmer codes found in Salesforce",
+       warning("At least one farmer code already in Salesforce", call. = FALSE))
 
-# Check that field officers exist in Salesforce (FIX THIS WITH UNIQUE FIELD)
+# Check that field officers exist in Salesforce (FIX THIS WHEN UNIQUE FIELD!!!)
 # Retrieve field officers names
-fo.sf.fields <- c("Name", "Id", "IsActive")
+fo.sf.fields <- c("Name", "Id", "IsActive", "Email")
 fo.sf <- rforcecom.retrieve(session, "User", fo.sf.fields)
 # Check that Field officers emails are unique
 ifelse(length(fo.sf$Name) == length(unique(fo.sf$Name)),
        "No duplicated Field officers",
        warning("There are duplicated Field officers in Salesforce. Please check",
                call. = FALSE))
+fo.sf[duplicated(fo.sf$Name), ] # Find duplicated field officer names
+fo.sf[fo.sf$Name == "Hasdir Hasdir", ] # List duplicated field officer
+fo.sf <- fo.sf[fo.sf$Id != "00528000004BKRFAA4", ] # Remove a FO from list
+# Run again first line above (ifelse(length(fo.sf$...)))
+
 # Check the FOs assigned to farmers that will be loaded exist in Salesforce
-fo.load <- unique(load$assigned.to)
-fo.load.in.sf <- fo.load %in% fo.sf$Name
-ifelse(length(fo.load.in.sf[fo.load.in.sf == FALSE]) == 0,
+fo.to.load <- unique(to.load$assigned.to) # list of FO in farmers to load
+fo.to.load.in.sf <- fo.to.load %in% fo.sf$Name # check if exist in Salesforce
+ifelse(length(fo.to.load.in.sf[fo.to.load.in.sf == FALSE]) == 0,
        "All field officers assigned exist in Salesforce",
        warning("There are field officers that were not found in Salesforce",
                call. = FALSE))
+fo.to.load[fo.to.load.in.sf == FALSE] # List FOs not found
+View(fo.sf[order(as.character(fo.sf$Name)), ]) # List FOs in SF
 
 # Assign field officers ID in assigned to
-load <- left_join(load, select(fo.sf, Name, Id),
+to.load <- left_join(to.load, select(fo.sf, Name, Id),
                   by = c("assigned.to" = "Name"))
-assigned <- is.na(load$Id)
-ifelse(length(assigned[assigned == FALSE]) == nrow(load),
-       "All assigned.to successfully assigned",
+assigned <- is.na(to.load$Id)
+ifelse(length(assigned[assigned == FALSE]) == nrow(to.load),
+       "All farmers to load successfully assigned",
        warning("There are assigned.to blank. Please check.", call. = FALSE))
 # Change name to assign.to Id and replace assigned.to for this variable
-load <- select(load, farmer.name, Id, farmer.code:renovation.ha)
-names(load)[names(load) == "Id"] <- "assigned.to"
+to.load <- select(to.load, farmer.name, Id, farmer.code:renovation.ha)
+names(to.load)[names(to.load) == "Id"] <- "assigned.to"
 
-# Check that year variables contain only the year
-date.vars <- c("farmer.birthday", "spouse.bday", "year.relationship.start")
 # farmer.birthday
-fb.chars <- nchar(as.character(load$farmer.birthday))
-ifelse(max(fb.chars) == min(fb.chars), 
-       paste("All fields have ", max(fb.chars), " characters.", sep = ""),
-       warning(paste("Values with different characters. Max = ", max(fb.chars),
-             ", Min = ", min(fb.chars), ".", sep = ""), call. = FALSE))
-load$farmer.birthday <- as.integer(substr(as.character(load$farmer.birthday),
+to.load$farmer.birthday #visual inspection
+# If date imported as integer (integer with 5 digits), run:
+to.load$farmer.birthday <- as.Date(to.load$farmer.birthday, origin = "1899-12-30")
+# If date contains day and month, remove:
+to.load$farmer.birthday <- as.integer(substr(as.character(to.load$farmer.birthday),
                                           1, 4))
+summary(to.load$farmer.birthday) # summary
+# Remove data for farmers born before 1930 and after 2002
+to.load$farmer.birthday <- ifelse(to.load$farmer.birthday < 1930 | 
+                                       to.load$farmer.birthday > 2002, 
+                                  NA, to.load$farmer.birthday)
+# Summarize (last step to check)
+summary(to.load$farmer.birthday)
+
 # spouse.bday
-sb.chars <- nchar(as.character(load$spouse.bday))
-ifelse(max(sb.chars, na.rm = TRUE) == min(sb.chars,  na.rm = TRUE), 
-       paste("All fields have ", max(sb.chars,  na.rm = TRUE), " characters.",
-             sep = ""),
-       warning(paste("Values with different characters. Max = ",
-                     max(sb.chars,  na.rm = TRUE), ", Min = ",
-                     min(sb.chars,  na.rm = TRUE), ".", sep = ""),
-               call. = FALSE))
-load$spouse.bday <- as.integer(substr(as.character(load$spouse.bday),
-                                          1, 4))
+to.load$spouse.bday #visual inspection
+# If date imported as integer (integer with 5 digits), run:
+to.load$spouse.bday <- as.Date(to.load$spouse.bday, origin = "1899-12-30")
+# If date contains day and month, remove:
+to.load$spouse.bday <- as.integer(substr(as.character(to.load$spouse.bday),
+                                             1, 4))
+summary(to.load$spouse.bday) # summary
+# Remove data for farmers born before 1930
+to.load$spouse.bday <- ifelse(to.load$spouse.bday < 1930, 
+                              NA, to.load$spouse.bday)
+# Summarize (last step to check)
+summary(to.load$spouse.bday)
+
 # year.relationship.start
-yrs.chars <- nchar(as.character(load$years.relationship.start))
-ifelse(max(yrs.chars, na.rm = TRUE) == min(yrs.chars,  na.rm = TRUE), 
-       paste("All fields have ", max(yrs.chars,  na.rm = TRUE), " characters.",
-             sep = ""),
-       warning(paste("Values with different characters. Max = ",
-                     max(yrs.chars,  na.rm = TRUE), ", Min = ",
-                     min(yrs.chars,  na.rm = TRUE), ".", sep = ""),
-               call. = FALSE))
-load$years.relationship.start <- as.integer(substr(as.character(load$years.relationship.start),
-                                      1, 4))
+to.load$year.relationship.start #visual inspection
+# If date imported as integer (integer with 5 digits), run:
+to.load$year.relationship.start <- as.Date(to.load$year.relationship.start, origin = "1899-12-30")
+# If date contains day and month, remove:
+to.load$year.relationship.start <- as.integer(substr(as.character(to.load$year.relationship.start),
+                                         1, 4))
+summary(to.load$year.relationship.start) # summary
+# Remove data for farmers born before 1930
+to.load$year.relationship.start <- ifelse(to.load$year.relationship.start < 1930, 
+                              NA, to.load$year.relationship.start)
+# Summarize (last step to check)
+summary(to.load$year.relationship.start)
 
-# Minimum and maximum values in date fields
-# Farmer birthday
-min(load$farmer.birthday, na.rm = TRUE)
-max(load$farmer.birthday, na.rm = TRUE)
-load$farmer.birthday <- ifelse(load$farmer.birthday == 0, NA,
-                               load$farmer.birthday)
-# Spouse birthday
-min(load$spouse.bday, na.rm = TRUE)
-max(load$spouse.bday, na.rm = TRUE)
-load$spouse.bday <- ifelse(load$spouse.bday == 0, NA, load$spouse.bday)
-# Year relationship start
-min(load$year.relationship.start)
-max(load$year.relationship.start)
-load$year.relationship.start <- ifelse(load$year.relationship.start == 0, NA,
-                                       load$year.relationship.start)
+# Gender
+table(to.load$gender)
+# L=male, P=female
+to.load$gender <- ifelse(to.load$gender == "L", "Male", "Female")
+
+# Educational level
+# Farmer
+educ.levels.load <- as.data.frame(table(to.load$educational.level))
+educ.levels.load
+educ.levels.correct <- q.options$option[q.options$short.name == 
+                                             "educational.level"]
+# Spouse
+sp.educ.levels.load <- as.data.frame(table(to.load$spouse.education))
+sp.educ.levels.load
+educ.levels.correct <- q.options$option[q.options$short.name == 
+                                             "educational.level"]
+
+# Have spouse
+table(to.load$spouse)
+# Duda=widow, Janda=widow, if any of these:
+wrong.spouse <- c("Duda", "Janda") # List of wrong options
+to.load$spouse <- ifelse(to.load$spouse %in% wrong.spouse,
+                         "No", to.load$spouse)
+
+# Farmer group (check if there seem  to be misspellings -i.e. very similarly
+# spelling groups)
+groups <- as.data.frame(table(to.load$farmer.group))
+groups[order(groups$Var1), ]
+
+# Farm certifications
+table(to.load$certifications)
+
+# Farm area in cocoa
+table(to.load$cocoa.cultivation.ha)
+
+# Final visual inspection to all data
+sapply(to.load, table)
 
 
-# Load data to SF ----------------------------------------------------------
+# to.load data to SF ----------------------------------------------------------
 
-# Remove file variables
-select(load, -file)
 
 # Change variable names to API names
-names(load) <- questions.coding$api.name
+names(to.load) <- q.coding$api.name
 
-# Load data to Salesforce
+# to.load data to Salesforce
 # Create job
 job_info <- rforcecom.createBulkJob(session, 
                                     operation='insert', 
                                     object='FDP_submission__c')
-# Load data
+# to.load data
 batches_info <- rforcecom.createBulkBatch(session, 
                                           jobId=job_info$id, 
-                                          load, 
+                                          to.load, 
                                           multiBatch = TRUE, 
                                           batchSize = 50)
 
@@ -204,8 +241,14 @@ batches_detail <- lapply(batches_info,
 close_job_info <- rforcecom.closeBulkJob(session, jobId=job_info$id)
 
 
-# Test data loaded --------------------------------------------------------
 
+# Save copy of data loaded ------------------------------------------------
+
+copy.name <- paste("../3. Previously loaded/data_loaded_",
+                   substr(Sys.time(), 1, 10), ".csv",
+                   sep = "")
+copy <- rforcecom.retrieve(session, "FDP_submission__c", q.coding$api.name)
+write.csv(copy, copy.name)
 
 
 # Organize ----------------------------------------------------------------
@@ -213,6 +256,7 @@ close_job_info <- rforcecom.closeBulkJob(session, jobId=job_info$id)
 # Logout from Salesforce
 rforcecom.logout(session)
 
-# Move loaded files to 'Previously loaded' folder
-file.copy()
-file.remove()
+# Move to.loaded files to 'Previously to.loaded' folder
+file.copy(f.to.load,
+          paste("../3. Previously loaded/", f.to.load, sep = ""))
+file.remove(f.to.load)
